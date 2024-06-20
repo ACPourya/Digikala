d039689d4e2e93e0b84e7bfc5e7468a27b2f194d
ACPourya <145262068+ACPourya@users.noreply.github.com>
2024-06-20 23:05:00 +0330

    Update amazing.js

--- public/js/amazing.js
+++ public/js/amazing.js
@@ -1,3 +1,129 @@
+///////////Hiding nav bar by scrolling///////////
+function handleScrollForLargeScreens() {
+}
+
+function checkScreenSize() {
+}
+
+checkScreenSize();
+
+window.addEventListener('resize', checkScreenSize);
+
+///////////Hiding nav bar by scrolling///////////
+
+///////////Scroll top///////////
+window.addEventListener('scroll', function() {
+    var scrollToTopBtn = document.getElementById('scrollToTopBtn');
+  });
+  
+  document.getElementById('scrollToTopBtn').addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+///////////Scroll top///////////
+
+///////////Read more - Read less///////////
+
+let more = document.getElementById('show-more');
+more.addEventListener('click',function (){
+  let container = document.getElementById('container');
+  let text = document.getElementById('text');
+  if (container.offsetHeight === 125) {
+    container.style.height = 'auto';
+    container.classList.add('before:hidden');
+    container.classList.remove('before:block');
+    text.textContent  = 'بستن';
+  }else{
+    container.style.height = '125px';
+    container.classList.remove('before:hidden');
+    container.classList.add('before:block');
+    text.textContent  = 'مشاهده بیشتر';
+  }
+});
+
+/////////Read more - Read less/////////
+
+///////// Pop up search /////////
+function openSearchPopup() {
+  document.getElementById('searchPopup').classList.add('show');
+  document.body.classList.add('overflow-hidden');
+}
+
+function closeSearchPopup() {
+  document.getElementById('searchPopup').classList.remove('show');
+  document.body.classList.remove('overflow-hidden');
+}
+///////// Pop up search /////////
+
+///////// Mobile be hover as soon as categories hovered /////////
+document.addEventListener('DOMContentLoaded', () => {
+  const mainCategory = document.getElementById('category-main');
+  const subMobile = document.getElementById('sub-mobile');
+  const subMobileSvg = document.getElementById('sub-mobile-svg');
+  const subMobileDetails = document.getElementById('sub-mobile-details');
+  const otherCategories = document.querySelectorAll('#category-others');
+
+  mainCategory.addEventListener('mouseenter', () => {
+    console.log('main category selected with mobile');
+    subMobile.classList.add('bg-white', 'text-[#ef394e]', 'block');
+    subMobileSvg.classList.add('fill-[#ef394e]');
+    subMobileDetails.classList.remove('hidden');
+    subMobileDetails.classList.add('block');
+  });
+
+  mainCategory.addEventListener('mouseleave', () => {
+    console.log('main category unselected');
+    subMobile.classList.remove('bg-white', 'text-[#ef394e]', 'block');
+    subMobileSvg.classList.remove('fill-[#ef394e]');
+    subMobileDetails.classList.remove('block');
+    subMobileDetails.classList.add('hidden');
+  });
+
+  otherCategories.forEach(category => {
+    category.addEventListener('mouseenter', () => {
+      console.log('other categories selected');
+      subMobile.classList.remove('bg-white', 'text-[#ef394e]', 'block');
+      subMobileSvg.classList.remove('fill-[#ef394e]');
+      subMobileDetails.classList.remove('block');
+      subMobileDetails.classList.add('hidden');
+    });
+
+    category.addEventListener('mouseleave', () => {
+      console.log('other categories unselected');
+      if (mainCategory.matches(':hover')) {
+        subMobile.classList.add('bg-white', 'text-[#ef394e]', 'block');
+        subMobileSvg.classList.add('fill-[#ef394e]');
+        subMobileDetails.classList.remove('hidden');
+        subMobileDetails.classList.add('block');
+      }
+    });
+  });
+});
+///////// Mobile be hover as soon as categories hovered /////////
+
+///////// Footer Mobile /////////
+function toggleSection(sectionId) {
+  var content = document.getElementById('collapsibleContent-' + sectionId);
+  var icon = document.getElementById('toggleIcon-' + sectionId);
+  if (content.classList.contains('hidden')) {
+      content.classList.remove('hidden');
+      icon.classList.remove('rotate-0');
+      icon.classList.add('rotate-180');
+      if(content.id === 'collapsibleContent-section4'){
+        content.classList.remove('pb-4');
+      }
+  } else {
+    if(content.id === 'collapsibleContent-section4'){
+      content.classList.add('pb-4');
+    }
+      content.classList.add('hidden');
+      icon.classList.remove('rotate-180');
+      icon.classList.add('rotate-0');
+  }
+}
+///////// Footer Mobile /////////
 /////////My first slider/////////
 /////////Amazing's slides/////////
 let fetchCategoryShopingSlider = async () => {
